fc3765479a9585107688806c269837a5b44bd155
rybaaa <bolshayaryba22@gmail.com>
2023-02-15 12:47:35 +0300

    #3

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from "@/routes/routes";
-import 'vue3-simple-typeahead/dist/vue3-simple-typeahead.css'
-import SimpleTypeahead from 'vue3-simple-typeahead';
 
 
 createApp(App)
     .use(store)
-    .use(SimpleTypeahead)
     .use(router)
     .mount('#app')
